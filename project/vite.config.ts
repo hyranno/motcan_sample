df3325746358f68ddc81c53701b81bf316b39e79
hyranno <hyranno4pub@gmail.com>
2024-05-16 13:12:20 +0000

    change vite config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,9 @@ import motionCanvas from '@motion-canvas/vite-plugin';
 import ffmpeg from '@motion-canvas/ffmpeg';
 
 export default defineConfig({
+  server: {
+    host: true
+  },
   plugins: [
     motionCanvas(),
     ffmpeg(),
