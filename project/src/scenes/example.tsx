948b6c292fb0ed33cb03e46e4b27c2e65de4f2a5
hyranno <hyranno4pub@gmail.com>
2024-05-24 08:59:29 +0000

    add time logging

--- project/src/scenes/example.tsx
+++ project/src/scenes/example.tsx
@@ -1,19 +1,25 @@
 import {Circle, makeScene2D} from '@motion-canvas/2d';
-import {ThreadGenerator, all, createRef, useDuration, waitFor, waitUntil} from '@motion-canvas/core';
+import {ThreadGenerator, all, createRef, useDuration, useLogger, useTime, waitFor, waitUntil} from '@motion-canvas/core';
+
+var timings: number[] = [];
 
 export default makeScene2D(function* (view) {
   // Create your animations here
+  timings = [];
 
   const circle = createRef<Circle>();
 
   view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
 
+  timings.push(useTime());
   yield* circle().scale(2, useDuration('event0')).to(1, 2);
+  timings.push(useTime());
   yield* waitUntil('event1');
   yield* all(
     generator(circle()),
     waitGenerator(),
   );
+  useLogger().info(timings.toString());
 });
 
 
@@ -22,5 +28,6 @@ function* generator(circle: Circle): ThreadGenerator {
 }
 
 function* waitGenerator(): ThreadGenerator {
+  timings.push(useTime());
   yield* waitFor(6);
 }
